4f846c899655bb69b3db2a0e3cc6c938f83e17b1
Kevin Li <kevinli1226@gmail.com>
2020-05-03 21:12:34 -0700

    add data in cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,76 @@
-import * as functions from 'firebase-functions';
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const axios = require('axios').default
+const newsCredentials = '3f5ecb41c72d4ea5a059d84cc87988b9'
+
+interface Source {
+    id: string;
+    name: string
+}
+
+interface rawHeadline {
+    source: Source;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+interface formattedHeadline {
+    source: string;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+
+async function getHeadlines() {
+    try {
+        const raw: any = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${newsCredentials}`)
+        const response: any = raw.data
+        if (response.status !== 'ok') {
+            throw new Error("Error. Unexepcted response status")
+        } else {
+            return response.articles
+        }
+    } catch (error) {
+        return error
+    }
+}
+
+function formatHeadlines(headlines: rawHeadline[]): formattedHeadline[] {
+    const formatted: formattedHeadline[] = headlines.map(headline => {
+        return {
+            ...headline,
+            source: headline.source.name, // format source to get string description
+        }
+    })
+    return formatted
+}
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
+  const resp: rawHeadline[] = await getHeadlines();
+  console.log(resp)
+
+  const formattedResp: formattedHeadline[] = formatHeadlines(resp);
+  console.log(formattedResp)
+
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.database().ref('/messages').push({original: formattedResp});
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
