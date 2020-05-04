1f0e7221a586c5be98d9caf2acea1a0b839dbaba
Kevin Li <kevinli1226@gmail.com>
2020-05-04 11:51:31 -0700

    change to use firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
+// Reference to our firestore database
+const db = admin.firestore();
 
 const axios = require('axios').default
 //TO-DO: move to environment variables
@@ -28,8 +30,6 @@ interface formattedHeadline {
     source: string;
     author: string;
     title: string;
-    description: string;
-    url: string;
     urlToImage: string;
     publishedAt: string;
     content: string;
@@ -46,26 +46,48 @@ async function getHeadlines() {
             return response.articles
         }
     } catch (error) {
-        return error
+        console.log('Failed in getHeadlines')
+        throw error
     }
 }
 
 function formatHeadlines(headlines: rawHeadline[]): formattedHeadline[] {
     const formatted: formattedHeadline[] = headlines.map(headline => {
         return {
-            ...headline,
             source: headline.source.name, // format source to get string description
+            author: headline.author,
+            title: headline.title,
+            urlToImage: headline.urlToImage,
+            publishedAt: headline.publishedAt,
+            content: headline.content,
         }
     })
+    // TO-DO: group by sources
     return formatted
 }
 
-exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
-  const resp: rawHeadline[] = await getHeadlines();
-  const formattedResp: formattedHeadline[] = formatHeadlines(resp);
+// TO-DO: make this into a scheduled pubsub function
+exports.getHeadlines = functions.https.onRequest(async (req: any, res: any) => {
+    try {
+        // raw unformatted headlines
+        const resp: rawHeadline[] = await getHeadlines();
 
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin.database().ref('/messages').push({headlines: formattedResp});
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
+        // minimized and aggregated headlines, grouped by source
+        const formattedResp: formattedHeadline[] = formatHeadlines(resp);
+
+        // YYYY-MM-DD document. One per day, updated hourly
+        const datetime: string = new Date().toISOString().slice(0,10).toString()
+
+        // upsert into headlines
+        const docRef: any = db.collection('headlines').doc(datetime)
+        docRef.set({
+            ...formattedResp,
+        })
+        console.log(`SUCCESS: upserted records for ${datetime}`)
+    
+        // TO-DO: remove dummy direction when integrated into pubsub
+        res.redirect('https://www.google.com/')
+    } catch (error) {
+        console.log(`FAILURE: ${error.toString()}`)
+    }
 });
